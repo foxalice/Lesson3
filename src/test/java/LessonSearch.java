c6b0f87effe8d096abf143fcb8d69d7f902e1a67
Saveleva Natalia <n.saveleva@itssupport.ru>
2023-01-12 15:50:25 +0400

    Task 2

--- src/test/java/LessonSearch.java
+++ src/test/java/LessonSearch.java
@@ -1,8 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class LessonSearch {
@@ -15,6 +14,16 @@ public class LessonSearch {
         $(withText("Enterprise")).click();
         $(".h1-mktg").shouldHave(text("Build like the best"));
 
+        sleep(5000);
+    }
+
+    @Test
+    void DrugAndDropTest() {
+
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        sleep(5000);
+        $(byId("column-a")).dragAndDropTo($(byId("column-b")));
+        $(byId("column-a")).shouldHave(text("B"));
         sleep(5000);
     }
 }
