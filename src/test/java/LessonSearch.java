ac28d563803d6c5783fe05a127456d037f472e68
Saveleva Natalia <n.saveleva@itssupport.ru>
2023-01-12 15:38:36 +0400

    Lesson 3 / Task 1

---
+++ src/test/java/LessonSearch.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class LessonSearch {
+    @Test
+    void FindHoverAndClick() {
+
+        open("https://github.com");
+
+        $(".header-menu-wrapper").$(byText("Solutions")).hover();
+        $(withText("Enterprise")).click();
+        $(".h1-mktg").shouldHave(text("Build like the best"));
+
+        sleep(5000);
+    }
+}
